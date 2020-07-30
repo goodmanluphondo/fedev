4e43d47f3c357963f29687a49e5547097a9b4f28
Goodman Luphondo <luphondog@icloud.com>
2020-07-30 17:29:41 +0200

    Modals

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="modal-wrapper">
-    <div class="modal">
-      <div class="modal-close" @click="closeModal()">&times;</div>
-      <div v-if="currentModalScreen == 1">
+  <div v-if="currentModalScreen == 1">
+    <div class="modal-wrapper">
+      <div class="modal">
+        <div class="modal-close" @click="closeModal()">&times;</div>
         <div class="modal-title">
           <h2>Project Rollback</h2>
-          <p>Choose a deployment to rollback.</p>
+          <span class="modal-title-description"
+            >Choose a deployment to roll back to</span
+          >
         </div>
         <div class="modal-body">
           <ul class="list">
@@ -66,13 +68,33 @@
           >
         </div>
       </div>
-      <div v-else-if="currentModalScreen == 2">
+    </div>
+  </div>
+
+  <div v-else-if="currentModalScreen == 2">
+    <div class="modal-wrapper">
+      <div class="modal">
+        <div class="modal-close" @click="closeModal()">&times;</div>
         <div class="modal-title">
           <h2>Are you sure?</h2>
-          <p>Are you sure you want to roll back?</p>
+          <span class="modal-title-description"
+            >Are you sure you want to roll back?</span
+          >
         </div>
         <div class="modal-body">
-          <div class="modal-deployment"></div>
+          <div class="modal-deployment">
+            <div class="modal-deployment-user">
+              <h4>Pippin Took</h4>
+              <p>
+                This is a generc commit message to be replaced by a real commit
+                message that contains details of the commit.
+              </p>
+            </div>
+            <div class="modal-deployment-info">
+              <div class="modal-commit-time">15:25 &emsp; 25 09 2019</div>
+              <div class="modal-commit-number">#7a7df2e</div>
+            </div>
+          </div>
         </div>
         <div class="modal-options">
           <a href="#" class="modal-btn" @click="stepBack()">No, Go Back</a>
@@ -84,7 +106,28 @@
           >
         </div>
       </div>
-      <div v-else></div>
+    </div>
+  </div>
+
+  <div v-else-if="currentModalScreen == 3">
+    <div class="modal-wrapper">
+      <div class="modal">
+        <div class="modal-close" @click="closeModal()">&times;</div>
+        <div class="modal-title">
+          <div class="modal-icon">
+            <div class="modal-icon-check"></div>
+          </div>
+          <h2>Rollback successful</h2>
+          <span class="modal-title-description"
+            >Fixed LTE has been rolled back successfully</span
+          >
+        </div>
+        <div class="modal-body">
+          <a href="#" class="modal-btn modal-btn-inverted" @click="closeModal()"
+            >Go to Project</a
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -97,6 +140,9 @@ export default {
     stepBack() {
       if (this.currentModalScreen == 1) {
         this.$store.dispatch("closeModal");
+      } else {
+        let screen = this.currentModalScreen - 1;
+        this.$store.dispatch("updateModalScreen", screen);
       }
     },
     stepForward() {
@@ -123,7 +169,7 @@ export default {
   z-index: 1000;
   display: block;
   position: fixed;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(17, 20, 29, 0.9);
 }
 .modal {
   width: 680px;
@@ -153,8 +199,37 @@ export default {
   color: #cdddff;
   background-color: rgba(61, 69, 88, 0.3);
 }
+.modal-title {
+  margin: 0;
+  margin-bottom: 30px;
+}
 .modal-title h2 {
   margin: 0;
+}
+.modal-title-description {
+  font-size: 20px;
+  font-weight: 400;
+}
+.modal-icon {
+  width: 90px;
+  height: 90px;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 30px;
+  border: 2px solid #ff3b70;
+  border-radius: 50%;
+  position: relative;
+}
+.modal-icon-check {
+  top: 9px;
+  left: 26px;
+  width: 33px;
+  height: 52px;
+  display: block;
+  border: solid #ff3b70;
+  border-width: 0px 2px 2px 0px;
+  transform: rotate(45deg);
+  position: absolute;
 }
 .modal-body {
   position: relative;
@@ -179,8 +254,20 @@ export default {
 .modal-deployment {
   width: 100%;
   display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 10px;
   border: 1px solid #647291;
   background-color: #171c27;
+}
+.modal-deployment-user {
+  padding: 10px;
+  text-align: left;
+}
+.modal-deployment-info {
+  width: 250px;
+  padding: 10px;
+  text-align: right;
 }
 .modal-options {
   left: 0;
@@ -194,6 +281,14 @@ export default {
   position: absolute;
   z-index: 1001;
   background-color: rgba(61, 69, 88, 0.3);
+}
+.modal-commit-time {
+  display: block;
+  font-size: 12px;
+  font-weight: 400;
+  margin: 0;
+  margin-bottom: 24px;
+  line-height: 28px;
 }
 .modal-commit-user {
   display: inline-block;
